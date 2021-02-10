4648feb143b46c3edc60331e44a393f0d101feca
Chibuokem Ibezim <chibuokemibezim@gmail.com>
2021-02-10 18:00:14 +0100

    Formatted code
    Added spaces and edited some functions

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -8,80 +8,85 @@ use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-  
 
-   function __construct()
-   {
-       $this->middleware('auth',['except'=>['ViewAllBlogs','BlogDetail']]);
-   }
-
-   function blogPage(){
-       return view('cundy.newposts');
-   }
-   function postblogDB(Request $request){
-
-    if(!empty($request->file('blogimg'))){
-        $filePath = $request->file('blogimg')->store('public/blog/img'); 
-    }else{
-        $filePath = " ";
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['ViewAllBlogs', 'BlogDetail']]);
     }
-      
-       $imgName = explode('/' , $filePath);
-       
-       $blog = new Blog;
-       $blog->blog_title=$request->blogtitle;
-       $blog->blog_post=$request->blogpost;
-       $blog->image= end($imgName);
-       if($blog->save()){
-        return redirect()->route('newposts')->with('status', 'Blog Post has been Sucessfully Added');
-       }
-       
-      
-   }
-   
-   function ShowBlog(){
-    $data = Blog::all();
-    return view('cundy.allpost', ['allblogs'=>$data]);
-   }
-   
-   function EditBlog($id){
-    $data = Blog::find($id); 
-    return view('cundy.editpost', ['postedit'=>$data]);    
-   }
-   
-   function UpdateBlog(Request $req){
-    if(!empty($req->file('blogimg'))){
-        $filePath = $req->file('blogimg')->store('public/blog/img');
-        $imgName = end(explode('/' , $filePath));
-    }else{
-        $imgName = " ";
+
+    public function blogPage()
+    {
+        return view('cundy.newposts');
     }
-    
-       $data = Blog::find($req->id);
-       $data -> blog_title = $req->blogtitle;
-       $data -> blog_post = $req->blogpost;
-       $data -> image = $imgName;
-       
-       if($data->save()){
-           return redirect()->route('allpost')->with('status', 'Blog Post has been Successfully Edited');
-       }
-   
-   }
-   
-   function DeleteBlog($id){
+
+    public function postblogDB(Request $request)
+    {
+
+        if (!empty($request->file('blogimg'))) {
+            $filePath = $request->file('blogimg')->store('public/blog/img');
+        } else {
+            $filePath = " ";
+        }
+
+        $imgName = explode('/', $filePath);
+
+        $blog = new Blog();
+        $blog->blog_title = $request->blogtitle;
+        $blog->blog_post = $request->blogpost;
+        $blog->image = end($imgName);
+        if ($blog->save()) {
+            return redirect()->route('newposts')->with('status', 'Blog Post has been Sucessfully Added');
+        }
+    }
+
+    public function ShowBlog()
+    {
+        $data = Blog::all();
+        return view('cundy.allpost', ['allblogs' => $data]);
+    }
+
+    public  function EditBlog($id)
+    {
+        $data = Blog::find($id);
+        return view('cundy.editpost', ['postedit' => $data]);
+    }
+
+    public function UpdateBlog(Request $req)
+    {
+        if (!empty($req->file('blogimg'))) {
+            $filePath = $req->file('blogimg')->store('public/blog/img');
+            $imgName = end(explode('/', $filePath));
+        } else {
+            $imgName = " ";
+        }
+
+        $data = Blog::find($req->id);
+        $data->blog_title = $req->blogtitle;
+        $data->blog_post = $req->blogpost;
+        $data->image = $imgName;
+
+        if ($data->save()) {
+            return redirect()->route('allpost')->with('status', 'Blog Post has been Successfully Edited');
+        }
+    }
+
+    public function DeleteBlog($id)
+    {
         Blog::find($id)->delete();
         return redirect()->route('allpost')->with('status', 'Blog Post Deleted!!!!');
-   }
-   function ViewAllBlogs(){
-    $data = Blog::all();
-    return view('cundysmithpub.blog_grid', ['allblogs'=>$data]);
-   }
+    }
 
-   function BlogDetail($id){
-    $data = Blog::find($id); 
-    $data1 = Blog::all();
-    return view('cundysmithpub.blog_detail', ['posteddet'=>$data],['allPosts'=>$data1]);    
-   }
-  
-   
+    public function ViewAllBlogs()
+    {
+        $data = Blog::all();
+        return view('cundysmithpub.blog_grid', ['allblogs' => $data]);
+    }
+
+    public function BlogDetail($id)
+    {
+        $data = Blog::find($id);
+        $data1 = Blog::all();
+        $info = array('posteddet' => $data, 'allPosts' => $data1);
+        return view('cundysmithpub.blog_detail', $info);
+    }
 }
